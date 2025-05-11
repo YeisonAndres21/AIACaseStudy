79741664e4dd167fd16ccb2f4ba14bf12baddf58
YeisonAndres21 <105325084+YeisonAndres21@users.noreply.github.com>
2025-05-11 18:39:03 -0500

    Create Variables.tf
    Variables

---
+++ Terraform/Variables.tf
@@ -0,0 +1,29 @@
+variable "VpcName" {
+  description = "Nombre del VPC"
+  type        = string
+  default     = "CloudFirstVPC"
+}
+
+variable "VpcCidr" {
+  description = "Rango CIDR para el VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "PublicSubnets" {
+  description = "CIDRs de subredes públicas"
+  type        = list(string)
+  default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+
+variable "PrivateSubnets" {
+  description = "CIDRs de subredes privadas"
+  type        = list(string)
+  default     = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+}
+
+variable "EnableNatGateway" {
+  description = "Crear NAT Gateways"
+  type        = bool
+  default     = true
+}
