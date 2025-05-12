1245a9c98a37849a2d20dd9c174b854b6168447b
YeisonAndres21 <105325084+YeisonAndres21@users.noreply.github.com>
2025-05-11 19:02:12 -0500

    Create Main.tf
    Main del Vpc

---
+++ Terraform/Modules/Vpc/Main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "VpcModule" {
+  source            = "./"
+  VpcName           = var.VpcName
+  VpcCidr           = var.VpcCidr
+  PublicSubnets     = var.PublicSubnets
+  PrivateSubnets    = var.PrivateSubnets
+  EnableNatGateway  = var.EnableNatGateway
+}
